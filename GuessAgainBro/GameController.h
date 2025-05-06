e6dfccb84ea7dd787cdfd07194da77f76f18bbcd
Denis Olechnovic <116972879+Myshk-IN@users.noreply.github.com>
2025-05-06 13:57:09 +0300

    - Added a .txt file containing a list of valid words - Implemented logic to select today's word from the list - Added a check to validate if a received word exists in the list

--- GuessAgainBro/GameController.h
+++ GuessAgainBro/GameController.h
@@ -2,17 +2,50 @@
 #include "Controller.h"
 #include "httplib.h"
 #include "json.hpp"
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>  
 
 class GameController : public Controller {
 	using json = nlohmann::json;
+private:
+    std::vector<std::string> valid_words;
+    std::string todays_word;
+
+	void LoadWordsFromFile(const std::string& filename) {
+		std::ifstream file(filename);
+		std::string word;
+		while (file >> word) {
+			valid_words.push_back(word);
+		}
+		if (!valid_words.empty()) {
+			srand(static_cast<unsigned int>(time(nullptr)));
+			todays_word = valid_words[rand() % valid_words.size()];
+		} 
+		else {
+			throw std::runtime_error("Word list is empty.");
+		}
+	}
 public:
 	GameController(httplib::Server& srv) : Controller(srv) {
 		SetRoute("/game");
-		std::string todays_word = "guess";
-		Post("/word", [todays_word](const httplib::Request& req) {
+
+		LoadWordsFromFile("words.txt");
+		std::cout << "TESTING: today's word is " << todays_word << std::endl;
+
+		Post("/word", [this](const httplib::Request& req) {
 			try{
 				auto json_body = json::parse(req.body);
 				std::string word = json_body["word"];
+
+				// Check if word is valid
+				if (std::find(valid_words.begin(), valid_words.end(), word) == valid_words.end()) {
+					return std::make_pair(404, json({ {"error", "Word not in the list"} }).dump());
+				}
 
 				json result;
 				for (size_t i = 0; i < word.length(); ++i) {
